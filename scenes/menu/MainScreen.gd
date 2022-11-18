e8523bfb9588c07968f38df38899c040e4835064
Antônio Hugo <hugorplobo@gmail.com>
2022-11-18 12:46:58 -0300

    Add main menu scene

---
+++ scenes/menu/MainScreen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var lobby = preload("res://scenes/lobby/Lobby.tscn")
+var servers = preload("res://scenes/servers/Servers.tscn")
+
+func _on_ServerButton_button_up():
+	get_tree().change_scene_to(lobby)
+	NetworkServer.start_server()
+
+
+func _on_ConnectButton_button_up():
+	get_tree().change_scene_to(servers)
