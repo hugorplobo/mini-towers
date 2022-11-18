ddeddceb86ca5b6f6255fe096cd31ab184d75f06
Antônio Hugo <hugorplobo@gmail.com>
2022-11-18 12:46:44 -0300

    Update block scene

--- scenes/block/Block.gd
+++ scenes/block/Block.gd
@@ -7,6 +7,7 @@ onready var tween_collision = $CollisionTween
 export var type = 1
 var is_ragdoll = false
 var is_rotating = false
+var is_moving = false
 var target_angle = 0
 var collision_node: CollisionPolygon2D
 
@@ -24,31 +25,56 @@ func _integrate_forces(state):
 	
 	state.linear_velocity = Vector2(0, 100)
 	
-	if Input.is_action_pressed("left"):
-		state.transform = state.transform.translated(Vector2.LEFT)
-	elif Input.is_action_pressed("right"):
-		state.transform = state.transform.translated(Vector2.RIGHT)
-	elif Input.is_action_pressed("down"):
+	if not is_moving:
+		if Input.is_action_pressed("left"):
+			move_block(state, 10)
+		elif Input.is_action_pressed("right"):
+			move_block(state, -10)
+	if Input.is_action_pressed("down"):
 		state.linear_velocity = Vector2(0, 200)
-	
-	if Input.is_action_just_pressed("rotate_right"):
-		rotate_right()
+	if not is_rotating:
+		if Input.is_action_just_pressed("rotate_right"):
+			rotate_block(90)
+		elif Input.is_action_just_pressed("rotate_left"):
+			rotate_block(-90)
+	if Input.is_action_just_pressed("impulse_right"):
+		impulse_block(30)
+	elif Input.is_action_just_pressed("impulse_left"):
+		impulse_block(-30)
 
 func _on_Block_body_entered(_body):
 	if not is_ragdoll:
 		is_ragdoll = true
 		set_deferred("gravity_scale", 1)
+		set_deferred("applied_force", Vector2.ZERO)
+		set_deferred("linear_velocity", Vector2.ZERO)
 		set_deferred("contact_monitor", false)
 		emit_signal("on_touch")
 
-func rotate_right():
+func move_block(state: Physics2DDirectBodyState, amount: int):
+	is_moving = true
+	state.transform = state.transform.translated(Vector2.LEFT * amount)
+	yield(get_tree().create_timer(0.1), "timeout")
+	is_moving = false
+
+func rotate_block(angle: int):
 	tween_sprite.interpolate_property(
-		sprite_node, "rotation_degrees", sprite_node.rotation_degrees, sprite_node.rotation_degrees + 90, 0.5, Tween.TRANS_ELASTIC
+		sprite_node, "rotation_degrees", sprite_node.rotation_degrees, sprite_node.rotation_degrees + angle, 0.25, Tween.TRANS_ELASTIC
 	)
 	
 	tween_collision.interpolate_property(
-		collision_node, "rotation_degrees", collision_node.rotation_degrees, collision_node.rotation_degrees + 90, 0.5, Tween.TRANS_ELASTIC
+		collision_node, "rotation_degrees", collision_node.rotation_degrees, collision_node.rotation_degrees + angle, 0.25, Tween.TRANS_ELASTIC
 	)
 	
+	is_rotating = true
 	tween_collision.start()
 	tween_sprite.start()
+
+func impulse_block(amount: int):
+	add_central_force(Vector2.RIGHT * amount * 1000)
+	yield(get_tree().create_timer(0.05), "timeout")
+	if not is_ragdoll:
+		add_central_force(Vector2.LEFT * amount * 1000)
+
+func _on_SpriteTween_tween_all_completed():
+	is_rotating = false
